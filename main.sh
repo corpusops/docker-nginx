8e8a5660220159c375b82895db22b31e1e72750a
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2023-06-29 12:19:57 +0200

    refresh

--- main.sh
+++ main.sh
@@ -257,7 +257,8 @@ SKIPPED_TAGS="$SKIP_TF|$SKIP_MINOR_OS|$SKIP_NODE|$SKIP_DOCKER|$SKIP_MINIO|$SKIP_
 CURRENT_TS=$(date +%s)
 IMAGES_SKIP_NS="((mailhog|postgis|pgrouting(-bare)?|^library|dejavu|(minio/(minio|mc))))"
 
-SKIPPED_TAGS="nginx:1.[7-9]|nginx:1.21[.]|1.[0-9][0-9].[0-9]+"
+SKIPPED_TAGS="nginx:(1.24|1.23|1.22|1.10|1.14)|nginx:1.[7-9]|nginx:1.21[.]|1.[0-9][0-9].[0-9]+"
+SKIPPED_TAGS="nginx:1.[7-9]|nginx:1.21[.]|1.[0-9][0-9].[0-9]+|alpine3.17"
 
 default_images="
 library/nginx
